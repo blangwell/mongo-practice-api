5d5f8f80c430ff0eaf5b64a53273b22b1d5aefa8
blangwell <b.langwell@outlook.com>
2020-09-01 18:13:59 -0700

    [express] Add home route to index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// QUESTION : this middleware allows us to accept 
+// traditional form data as well as json ?
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Home route, homeboy')
+})
+
+app.listen(3000, () => {
+  console.log('List\'nin\' to Port 3000')
+})
